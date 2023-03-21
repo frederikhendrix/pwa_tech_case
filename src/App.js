f498381a20d025bf01dffc8f0be354a4301dc356
frederikhendrix <rikhendrix@live.nl>
2023-03-21 16:56:54 +0100

    dragging gesture added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useSpring, animated } from "@react-spring/web";
+import { useDrag, useGesture } from "@use-gesture/react";
+import { useRef } from "react";
 
 function App() {
+  let imageRef = useRef();
+
+  useGesture(
+    {
+      onDrag: () => {
+        console.log("dragging");
+      },
+    },
+    {
+      target: imageRef,
+    }
+  );
+
+  const [{ x, y }, api] = useSpring(() => ({ x: 0, y: 0 }));
+
+  // Set the drag hook and define component movement based on gesture data
+  const bind = useDrag(({ down, movement: [mx, my] }) => {
+    api.start({ x: down ? mx : 0, y: down ? my : 0, immediate: down });
+  });
+
+  // Bind it to a component
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img ref={imageRef} src="/logo192.png" />
     </div>
   );
 }
